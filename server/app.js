f2eb895c38439caddfd3de91ad6d049355333398
Sulav Neupane <neupsulav@gmail.com>
2024-04-02 09:36:13 +0545

    Added user signup API with image upload

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const dotenv = require("dotenv");
 const errorHandlerMiddleware = require("./middlewares/ErrorHandlerMiddleware");
 const noRoute = require("./middlewares/noRoute");
 const connectDatabase = require("./db/connect");
+const authRouters = require("./routers/auth");
 
 const app = express();
 dotenv.config();
 const PORT = process.env.port || 3000;
 
 app.use(express.json());
+app.use("/api/auth", authRouters);
 
 // error handler middlewares
 app.use(errorHandlerMiddleware);
