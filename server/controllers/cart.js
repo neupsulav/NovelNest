78e34304fda384a54c81f2b210e642e1e12f7d63
Sulav Neupane <neupsulav@gmail.com>
2024-04-02 12:14:15 +0545

    Added authentication middleware to verify token while calling API

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -2,9 +2,14 @@ const ErrorHandler = require("../middlewares/errorHandler");
 const catchAsync = require("../middlewares/catchAsync");
 const Cart = require("../models/Cart");
 const Product = require("../models/Product");
+const mongoose = require("mongoose");
 
 // to add items to cart
 const addItemsToCart = catchAsync(async (req, res, next) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return next(new ErrorHandler("Invalid product ID", 401));
+  }
+
   const productId = req.params.id;
   const userId = req.user.userId;
 
--- server/routers/cart.js
+++ server/routers/cart.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const authentication = require("../middlewares/authentication");
 
 const { addItemsToCart } = require("../controllers/cart");
 
 // routes
-router.post("/addcartitem/:id", addItemsToCart);
+router.post("/addcartitem/:id", authentication, addItemsToCart);
 
 module.exports = router;
